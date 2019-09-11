f0ad901e420e1b4d91905476f72dd210222e163f
Androz <androz2091@gmail.com>
2019-09-11 20:18:21 +0200

    Update syntax

--- examples/createUser.js
+++ examples/createUser.js
@@ -2,12 +2,8 @@ const Node = require('nodeactyl');
 
 exports.run = async (client, message, args, level, Node) => {
 
-    let username = args[0];
-    let email = args[1];
-    let firstname = args[2];
-    let lastname = args[3];
-    let password = args[4];
-    if(!args[0]) return message.channel.send("Une erreur de syntaxe s'est produite, merci de donner dans l'ordre suivante : username, email, firstname, lastname, password !")
+    let username, email, firstname, lastname, password = args;
+    if(!username) return message.channel.send("Une erreur de syntaxe s'est produite, merci de donner dans l'ordre suivante : username, email, firstname, lastname, password !")
 
     Node.login("http://panel.mypanel.com", "gjcum5VWs4363PY6A4L");
     Node.createUser("Username", "Email", "FirstName", "LastName", "Password");
