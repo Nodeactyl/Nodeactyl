4cbd981048cb764850d00b81df994550c5c69aa4
Dalton Burchard <48583030+Burchard36@users.noreply.github.com>
2020-01-24 00:33:09 -0800

    Fix syntax issue with fastLogin

--- client/index.js
+++ client/index.js
@@ -49,7 +49,7 @@ function login(HOST, KEY, callback) {
     });
 }
 
-function fastLogin() {
+function fastLogin(HOST, KEY) {
     console.log("NODEACTYL WARNING: There has been a COMPLETE rewrite of Nodeactyl, Please review our new documentation at: https://nodeactyl.elliotfrost.xyz/v/v2.0.0-english/ Alternativly if you need further assistance ask for help in our discord server: https://discordapp.com/invite/3e5uJPt");
     process.env.CLIENT_NODEACTYL_HOST = HOST;
     process.env.CLIENT_NODEACTYL_KEY = KEY;
