9bd08fc424b14d7559889610ae3752864edaa850
Liyf4TakingLs <43788244+Liyf4TakingLs@users.noreply.github.com>
2021-01-07 22:52:36 +0100

    Server Pagination

--- application/methods/getAllServers.js
+++ application/methods/getAllServers.js
@@ -1,7 +1,13 @@
 const req = require('../ApplicationRequest.js');
 
-function getAllServers() {
+/**
+ * @param {Integer} page Specify a page, leave blank if you dont want to paginate
+ */
+
+
+function getAllServers(page) {
 	const Req = new req(process.env.APPLICATION_NODEACTYL_HOST, process.env.APPLICATION_NODEACTYL_KEY);
+	if(page !== undefined) return Req.getRequest('GetAllServersPagination',page);
 	return Req.getRequest('GetAllServers', null);
 }
 
