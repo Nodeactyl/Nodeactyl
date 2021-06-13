f7fed1711d67db2c4ae504bf8f2034366db199ab
cainthebest <75994858+cainthebest@users.noreply.github.com>
2021-06-13 21:31:19 +0100

    forgot app but added it all with default any

--- typings.d.ts
+++ typings.d.ts
@@ -1,4 +1,4 @@
-// Nodeactyl client typings
+// Nodeactyl client.js typings
 declare class Client {
   hostUrl: any;
   apiKey: any;
@@ -59,9 +59,9 @@ declare class Client {
   getBackupDownload(serverId: any, backupId: any): Promise<unknown>;
 
   deleteBackup(serverId: any, backupId: any): Promise<unknown>;
-  
+
   getSubUsers(serverId: any): Promise<unknown>;
-  
+
   createSubUser(serverId: any, email: any, permissions: any): Promise<unknown>;
 
   getApiKeys(): Promise<unknown>;
@@ -76,4 +76,131 @@ declare class Client {
   updateEmail(newEmail: any, currentPassword: any): Promise<unknown>;
 
   updatePassword(newPassword: any, currentPassword: any): Promise<unknown>;
+}
+
+// Nodeactyl application.js typings
+declare class Application {
+  hostUrl: any;
+  apiKey: any;
+
+  constructor(host: any, key: any);
+  processError(err: { status: undefined; statusCode: undefined }):
+    | number
+    | {
+        status: undefined;
+        statusCode: undefined;
+      }
+    | undefined;
+
+  getErrorCode(err: {
+    status?: undefined;
+    statusCode?: undefined;
+    message?: any;
+    charAt?: any;
+    length?: any;
+  }): number | undefined;
+
+  getAllUsers(): Promise<unknown>;
+
+  getUserDetails(userId: any): Promise<unknown>;
+
+  getUserByUsername(username: any): Promise<unknown>;
+
+  getUserByEmail(email: any): Promise<unknown>;
+
+  getUserPage(pageNum: any): Promise<unknown>;
+
+  createUser(
+    Email: any,
+    Username: any,
+    FirstName: any,
+    LastName: any
+  ): Promise<unknown>;
+
+  updateUserDetails(
+    userId: any,
+    Email: any,
+    Username: any,
+    FirstName: any,
+    LastName: any,
+    Language: any,
+    Password: any
+  ): Promise<unknown>;
+
+  createServer(
+    Version: any,
+    NameOfServer: any,
+    OwnerID: any,
+    EggID: any,
+    DockerImage: any,
+    StartupCmd: any,
+    RAM: any,
+    Swap: any,
+    Disk: any,
+    IO: any,
+    CPU: any,
+    AmountOfDatabases: any,
+    AmountOfBackups: any,
+    AmountOfAllocations: any
+  ): Promise<unknown>;
+
+  createSimpleServer(
+    OwnerID: any,
+    EggID: any,
+    RAM: any,
+    Disk: any,
+    CPU: any,
+    AmountOfDatabases: any,
+    AmountOfBackups: any,
+    AmountOfAllocations: any
+  ): Promise<unknown>;
+
+  getAllServers(): Promise<unknown>;
+
+  getServerPage(pageNum: any): Promise<unknown>;
+
+  getServerDetails(serverId: any): Promise<unknown>;
+
+  deleteUser(userId: any): Promise<unknown>;
+
+  suspendServer(serverId: any): Promise<unknown>;
+
+  unsuspendServer(serverId: any): Promise<unknown>;
+
+  reinstallServer(serverId: any): Promise<unknown>;
+
+  updateServerDetails(
+    serverId: any,
+    Name: any,
+    userId: any,
+    externalId: any,
+    Description: any
+  ): Promise<unknown>;
+
+  updateServerBuild(
+    serverId: any,
+    AllocationID: any,
+    RAM: any,
+    Swap: any,
+    IO: any,
+    CPU: any,
+    Disk: any,
+    Threads: any,
+    AmountOfDatabases: any,
+    AmountOfBackups: any,
+    AmountOfAllocations: any
+  ): Promise<unknown>;
+
+  updateServerStartup(
+    serverId: any,
+    StartupCmd: any,
+    Environment: any,
+    Egg: any,
+    DockerImage: any,
+    SkipScripts: any
+  ): Promise<unknown>;
+
+  deleteServer(serverId: any): Promise<unknown>;
+
+  getNestDetails(nestId: any): Promise<unknown>;
 }
