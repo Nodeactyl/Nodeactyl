84d1ccdf032587a1e19d5fcc7052114f0c5909b9
cainthebest <75994858+cainthebest@users.noreply.github.com>
2021-06-15 17:31:54 +0100

    added to one file and finalised

--- typings.d.ts
+++ typings.d.ts
@@ -22,7 +22,7 @@ declare module "nodeactyl" {
      * WARN: MUST USE ClientAPI Key. Application API Keys no longer work with ANY Pterodactyl V1 & above.
      *
      */
-    getAccountDetails(): Promise<Object>;
+    getAccountDetails(): Promise<object>;
     /**
      * Gets the permissions that this associated host and key have.
      *
@@ -68,7 +68,7 @@ declare module "nodeactyl" {
      *
      * @param serverId ID of the server you would like to start.
      */
-    startServer(serverId: interger): Promise<Boolean>;
+    startServer(serverId: interger): Promise<boolean>;
     /**
      * Stops a server if the host and api key have permission.
      *
@@ -76,7 +76,7 @@ declare module "nodeactyl" {
      *
      * @param serverId ID of the server you would like to stop.
      */
-    stopServer(serverId: interger): Promise<Boolean>;
+    stopServer(serverId: interger): Promise<boolean>;
     /**
      * Restarts a server if the host and api key have permission.
      *
@@ -84,7 +84,7 @@ declare module "nodeactyl" {
      *
      * @param serverId ID of the server you would like to restart.
      */
-    restartServer(serverId: interger): Promise<Boolean>;
+    restartServer(serverId: interger): Promise<boolean>;
     /**
      * Kills a server if the host and api key have permission.
      *
@@ -92,7 +92,7 @@ declare module "nodeactyl" {
      *
      * @param serverId ID of the server you would like to kill.
      */
-    killServer(serverId: interger): Promise<Boolean>;
+    killServer(serverId: interger): Promise<boolean>;
     /**
      * Sends a command to a server if the host and api key have permission
      * By default Pterodactyl API returns a empty string on success (""), The response is altered to make it a boolean value of "true".
@@ -100,7 +100,7 @@ declare module "nodeactyl" {
      * @param serverId ID of the server you would like to send a command.
      * @param command The command you are sending to target server.
      */
-    sendServerCommand(serverId: interger, command: any): Promise<Boolean>;
+    sendServerCommand(serverId: interger, command: any): Promise<boolean>;
     /**
      * gets a servers current memory/cpu/disk usages as bytes
      *
@@ -121,19 +121,19 @@ declare module "nodeactyl" {
      * @param serverId ID of the server that you would like to rename.
      * @param newName The new name of the target server.
      */
-    renameServer(serverId: interger, newName: string): Promise<Boolean>;
+    renameServer(serverId: interger, newName: string): Promise<boolean>;
     /**
      * ReInstalls a target server
      *
      * @param serverId ID of the target server that you would like to reinstall.
      */
-    reInstallServer(serverId: interger): Promise<Boolean>;
+    reInstallServer(serverId: interger): Promise<boolean>;
     /**
      * Lists what backups a server has
      *
      * @param serverId ID of the server that you would like to get a list of backups.
      */
-    listServerBackups(serverId: interger): Promise<Array<any>>;
+    listServerBackups(serverId: interger): Promise<any[]>;
     /**
      * Creates a backup for a specified server
      *
@@ -142,28 +142,28 @@ declare module "nodeactyl" {
      *
      * @param serverId ID of the server that you would like to create a back up.
      */
-    createServerBackup(serverId: interger): Promise<Object>;
+    createServerBackup(serverId: interger): Promise<object>;
     /**
      * Gets details about a specified server backups
      *
      * @param serverId ID of the server that you would like to get a specified backup.
      * @param backupId ID of the backup to get the details from.
      */
-    getBackupDetails(serverId: interger, backupId: interger): Promise<Object>;
+    getBackupDetails(serverId: interger, backupId: interger): Promise<object>;
     /**
      * Gets a clickable URL to download your server backup
      *
      * @param serverId ID of the server that you would like to get a specified backup from.
      * @param backupId ID of the backup to download.
      */
-    getBackupDownload(serverId: interger, backupId: interger): Promise<Object>;
+    getBackupDownload(serverId: interger, backupId: interger): Promise<object>;
     /**
      * Deletes a specified backup
      *
      * @param serverId ID of the server that you would like to delete a backup from.
      * @param backupId ID of the backup that you would like to delete.
      */
-    deleteBackup(serverId: interger, backupId: interger): Promise<Boolean>;
+    deleteBackup(serverId: interger, backupId: interger): Promise<boolean>;
     /**
      * Shows the sub users at the target server.
      *
@@ -212,7 +212,7 @@ declare module "nodeactyl" {
      *
      * @param keyId The key ID that you would like to delete.
      */
-    deleteApiKey(keyId: interger): Promise<Boolean>;
+    deleteApiKey(keyId: interger): Promise<boolean>;
     /**
      * Updates the email for the specified host and API key
      *
@@ -223,7 +223,7 @@ declare module "nodeactyl" {
      * @param newEmail The new email that you would like to set.
      * @param currentPassword The current password that you use.
      */
-    updateEmail(newEmail: string, currentPassword: string): Promise<Boolean>;
+    updateEmail(newEmail: string, currentPassword: string): Promise<boolean>;
     /**
      * Updates the password for the specified host and api key
      *
@@ -237,83 +237,31 @@ declare module "nodeactyl" {
     updatePassword(
       newPassword: string,
       currentPassword: string
-    ): Promise<Boolean>;
+    ): Promise<boolean>;
   }
   export class Application {
     hostUrl: string;
     apiKey: string;
 
-    /**
-     * Main application class constructor
-     * @param host Where your panel is hosted (EG: http(s)://panel.host.net/)
-     * @param key
-     */
-    constructor(host: URL, key: string);
-    /**
-     * Gets a list of users from your panel
-     *
-     * @returns {Promise<unknown>}
-     * $param {integer} Page number
-     */
+    constructor(host: string, key: string);
+
     getAllUsers(): Promise<unknown>;
-    /**
-     * Gets details of a user
-     *
-     * @returns {Promise<unknown>}
-     * $param {integer} userId
-     */
+
     getUserDetails(userId: interger): Promise<unknown>;
-    /**
-     * Gets details of a user by username
-     *
-     * @returns {Promise<unknown>}
-     * $param {string} username
-     */
+
     getUserByUsername(username: string): Promise<unknown>;
-    /**
-     * Gets details of a user
-     *
-     * @returns {Promise<unknown>}
-     * $param {string} email
-     */
+
     getUserByEmail(email: string): Promise<unknown>;
-    /**
-     * Gets users by a specified page number
-     *
-     * This will return an empty array if the specified page was invalid.
-     *
-     * @param pageNum
-     * @returns {Promise<unknown>}
-     */
+
     getUserPage(pageNum: interger): Promise<unknown>;
-    /**
-     * Creates a user
-     *
-     * @param {string} Username Users username
-     * @param {string} Email Users email
-     * @param {string} FirstName Users first name
-     * @param {string} LastName Users last name
-     * @returns {Promise<unknown>}
-     */
+
     createUser(
       Email: string,
       Username: string,
       FirstName: string,
       LastName: string
     ): Promise<unknown>;
-    /**
-     * UpdateUserDetails
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     *
-     * @param {integer} userId
-     * @param {string} Email
-     * @param {string} Username
-     * @param {string} FirstName
-     * @param {string} LastName
-     * @param {string} Language
-     * @param {string} Password
-     * @returns {Promise<Boolean>}
-     */
+
     updateUserDetails(
       userId: interger,
       Email: string,
@@ -322,26 +270,8 @@ declare module "nodeactyl" {
       LastName: string,
       Language: string,
       Password: string
-    ): Promise<Boolean>;
-    /**
-     * Creates a server
-     *
-     * @param {string} Version Version of the server to use
-     * @param {string} NameOfServer Name of server to create
-     * @param {integer} OwnerID User ID of who should own this server
-     * @param {integer} EggID Egg ID to use when installing the server
-     * @param {string} DockerImage The image to use from Docker
-     * @param {string} StartupCmd The command to use when starting this server (AKA JVM Arguments)
-     * @param {integer} RAM The amount of RAM the server has
-     * @param {integer} Swap The amount of Swap the server has
-     * @param {integer} Disk The amount of Storage the server has
-     * @param {integer} IO Set this to 500 please. (Even if you know what it is leave it alone)
-     * @param {integer} CPU The amount of CPU Power the server can use (100 = 1 core);
-     * @param {integer} AmountOfDatabases The max amount of databases a server can use
-     * @param {integer} AmountOfBackups The max backups you can hold
-     * @param {integer} AmountOfAllocations The max amount of allocation(s) a server can us
-     * @returns {Promise<unknown>}
-     */
+    ): Promise<boolean>;
+
     createServer(
       Version: string,
       NameOfServer: string,
@@ -358,19 +288,7 @@ declare module "nodeactyl" {
       AmountOfBackups: interger,
       AmountOfAllocations: interger
     ): Promise<unknown>;
-    /**
-     * Creates a server
-     *
-     * @param {integer} OwnerID User ID of who should own this server
-     * @param {integer} EggID Egg ID to use when installing the server
-     * @param {integer} RAM The amount of RAM the server has
-     * @param {integer} Disk The amount of Storage the server has
-     * @param {integer} CPU The amount of CPU Power the server can use (100 = 1 core);
-     * @param {integer} AmountOfDatabases The max amount of databases a server can use
-     * @param {integer} AmountOfBackups The max backups you can hold
-     * @param {integer} AmountOfAllocations The max amount of allocation(s) a server can us
-     * @returns {Promise<unknown>}
-     */
+
     createSimpleServer(
       OwnerID: interger,
       EggID: interger,
@@ -381,100 +299,29 @@ declare module "nodeactyl" {
       AmountOfBackups: interger,
       AmountOfAllocations: interger
     ): Promise<unknown>;
-    /**
-     * Gets a list of servers from your panel, currently this only get the first page but i will add support for grabbing ALL pages with this methods
-     *
-     * @returns {Promise<unknown>}
-     */
+
     getAllServers(): Promise<unknown>;
-    /**
-     * Gets servers by a specified page number
-     *
-     * This will return an empty array if the specified page was invalid.
-     *
-     * @param {integer} pageNum
-     * @returns {Promise<unknown>}
-     */
+
     getServerPage(pageNum: interger): Promise<unknown>;
-    /**
-     * Gets a info of a server from your panel
-     *
-     * @param {integer} serverId
-     * @returns {Promise<unknown>}
-     */
+
     getServerDetails(serverId: interger): Promise<unknown>;
-    /**
-     * Deletes a specified user
-     *
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     * However do not this value will NEVER be false. To catch an error for this request you check if the caught error === 404, this will mean
-     * the provided API key was non existing.
-     *
-     * MUST USE ClientAPI Key!!! Application API Keys NO LONGER WORK with ANY Pterodactyl version 1 and above!
-     *
-     * @param userId
-     * @returns {Promise<Boolean>}
-     */
-    deleteUser(userId: interger): Promise<Boolean>;
-    /**
-     * Suspend a server if the host and api key have permission
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     *
-     * @param {integer} serverId
-     * @returns {Promise<Boolean>}
-     */
-    suspendServer(serverId: interger): Promise<Boolean>;
-    /**
-     * Unsuspend a server if the host and api key have permission
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     *
-     * @param {integer} serverId
-     * @returns {Promise<Boolean>}
-     */
-    unsuspendServer(serverId: interger): Promise<Boolean>;
-    /**
-     * Reinstall a server if the host and api key have permission
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     *
-     * @param {integer} serverId
-     * @returns {Promise<Boolean>}
-     */
-    reinstallServer(serverId: interger): Promise<Boolean>;
-    /**
-     * UpdateServerDetails
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     *
-     * @param {integer} serverId
-     * @param {string} Name
-     * @param {integer} userId
-     * @param {integer} externalId
-     * @param {string} Description
-     * @returns {Promise<Boolean>}
-     */
+
+    deleteUser(userId: interger): Promise<boolean>;
+
+    suspendServer(serverId: interger): Promise<boolean>;
+
+    unsuspendServer(serverId: interger): Promise<boolean>;
+
+    reinstallServer(serverId: interger): Promise<boolean>;
+
     updateServerDetails(
       serverId: interger,
       Name: string,
       userId: interger,
       externalId: interger,
       Description: string
-    ): Promise<Boolean>;
-    /**
-     * UpdateServerBuild
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     *
-     * @param {integer} serverId
-     * @param {integer} AllocationID
-     * @param {integer} RAM
-     * @param {integer} Swap
-     * @param {integer} IO
-     * @param {integer} CPU
-     * @param {integer} Disk
-     * @param {integer} Threads
-     * @param {integer} AmountOfDatabases
-     * @param {integer} AmountOfBackups
-     * @param {integer} AmountOfAllocations
-     * @returns {Promise<Boolean>}
-     */
+    ): Promise<boolean>;
+
     updateServerBuild(
       serverId: interger,
       AllocationID: interger,
@@ -487,20 +334,8 @@ declare module "nodeactyl" {
       AmountOfDatabases: interger,
       AmountOfBackups: interger,
       AmountOfAllocations: interger
-    ): Promise<Boolean>;
-    /**
-     * UpdateServerStartup
-     *
-     * This update the startup details for a specified server
-     *
-     * @param serverId ID of serer
-     * @param StartupCmd new startup command to use for this server
-     * @param Environment The environment object for this server to use
-     * @param Egg Egg ID for this serer to use
-     * @param DockerImage Docker Image for this serer
-     * @param SkipScripts Do you want to skip scripts? (have no idea what this is)
-     * @returns {Promise<unknown>}
-     */
+    ): Promise<boolean>;
+
     updateServerStartup(
       serverId: interger,
       StartupCmd: string,
@@ -509,25 +344,9 @@ declare module "nodeactyl" {
       DockerImage: string,
       SkipScripts: any
     ): Promise<unknown>;
-    /**
-     * Deletes a specified server
-     *
-     * By default Pterodactyl API returns a empty string on success (""), i altered the response to make it a boolean value of "true"
-     * However do not this value will NEVER be false. To catch an error for this request you check if the caught error === 404, this will mean
-     * the provided API key was non existing.
-     *
-     * MUST USE ClientAPI Key!!! Application API Keys NO LONGER WORK with ANY Pterodactyl version 1 and above!
-     *
-     * @param serverId
-     * @returns {Promise<Boolean>}
-     */
-    deleteServer(serverId: interger): Promise<Boolean>;
-    /**
-     * Gets details of a nest
-     *
-     * @returns {Promise<unknown>}
-     * $param {integer} nestId
-     */
+
+    deleteServer(serverId: interger): Promise<boolean>;
+
     getNestDetails(nestId: interger): Promise<unknown>;
   }
 }
