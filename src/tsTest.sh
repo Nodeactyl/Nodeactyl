0ac12615334720980c5e1b7afb2ed85119913a13
cainthebest <75994858+cainthebest@users.noreply.github.com>
2021-06-17 03:56:59 +0100

    test for ts

---
+++ src/tsTest.sh
@@ -0,0 +1,9 @@
+TYPE_ERRORS="$(tsc --project ./tsconfig.json)"
+
+if [[ -n $TYPE_ERRORS ]]; then
+    echo "FOUND ERRORS"
+    echo $TYPE_ERRORS
+    echo "EXITING TEST"
+    exit
+fi
+    echo "NO ERRORS FOUND"
