a622076a84c7da0f440a93e8ce46fa10012d257f
cainthebest <75994858+cainthebest@users.noreply.github.com>
2021-06-27 18:11:13 +0100

    quick fix

--- src/client/clientIndex.ts
+++ src/client/clientIndex.ts
@@ -1,6 +1,3 @@
-import * as methods from '../methods/client';
-import Axios from '../utils/axiosRequest';
-
 export default class NodeactylClient {
     hostUrl: string;
     apiKey: string;
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-import { NodeactylApp, NodeactylClient } from './index';
-
-const app = new NodeactylApp('HostURL', 'AppApiKey');
-const client = new NodeactylClient('HostUrl', 'ClientApiKey');
