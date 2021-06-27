c59d5815aadc41c00858070e3adf09cbb00ea5eb
cainthebest <75994858+cainthebest@users.noreply.github.com>
2021-06-27 19:57:09 +0100

    fix test

--- src/app/appIndex.ts
+++ src/app/appIndex.ts
@@ -1,1 +1,9 @@
-//test
+export default class NodeactylApp {
+    hostUrl: string;
+    apiKey: string;
+
+    constructor(host: string, key: string) {
+        this.hostUrl = host;
+        this.apiKey = key;
+    }
+}
